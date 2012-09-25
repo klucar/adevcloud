6723cffcf49a5f016a9df6004a8b4765f34f63b2
Jim Klucar <klucar@gmail.com>
2012-09-25 16:45:43 -0400

    compilation of build steps from the README.md

---
+++ buildall.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+# just a compilation of the steps outlined in the README.md
+
+     cd hadoop-common
+     ant binary
+     tar xvf ./build/hadoop-0.20.203.1-SNAPSHOT-bin.tar.gz -C ../.
+     cd ..
+     cd zookeeper
+     ant binary
+     tar xvf ./build/zookeeper-3.3.6-bin.tar.gz -C ../.
+     cd ..
+     cd accumulo
+     mvn package && mvn assembly:single -N
+     tar xvf target/accumulo-1.4.1-dist.tar.gz -C ../.
+     cd ..
+
+
+
