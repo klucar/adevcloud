9a034ec1879a2afb7e25eddfcf4e44e9ac61c627
Jim Klucar <klucar@gmail.com>
2012-09-26 12:36:11 -0400

    setup some variables, initial sed line working

--- configall.sh
+++ configall.sh
@@ -2,17 +2,17 @@
 
 # figure out the install directory
 ADC_HOME=/home/jim/code/test/adevcloud
-ADC_HADOOP=${ADC_HOME}/hadoop-0.20.203.1-SNAPSHOT
-ADC_ZOOKEEPER=${ADC_HOME}/zookeeper-3.3.6
-ADC_ACCUMULO=${ADC_HOME}/accumulo-1.4.1
+ADC_HADOOP=$ADC_HOME/hadoop-0.20.203.1-SNAPSHOT
+ADC_ZOOKEEPER=$ADC_HOME/zookeeper-3.3.6
+ADC_ACCUMULO=$ADC_HOME/accumulo-1.4.1
 
 # Test to see if JAVA_HOME is set
 
 
 # Copy files from conf directories to their prospective build dirs
-cp ${ADC_HOME}/conf/hadoop/* ${ADC_HADOOP}/conf/.
-cp ${ADC_HOME}/conf/zookeeper/* ${ADC_ZOOKEEPER}/conf/.
-cp ${ADC_HOME}/conf/accumulo/* ${ADC_ACCUMULO}/conf/.
+cp $ADC_HOME/conf/hadoop/* $ADC_HADOOP/conf/.
+cp $ADC_HOME/conf/zookeeper/* $ADC_ZOOKEEPER/conf/.
+cp $ADC_HOME/conf/accumulo/* $ADC_ACCUMULO/conf/.
 
 #################################################
 # sed script the new values into the config files
@@ -20,7 +20,8 @@ cp ${ADC_HOME}/conf/accumulo/* ${ADC_ACCUMULO}/conf/.
 # hadoop_env.sh
 # XX_JAVA_HOME_XX
 echo "ADC_HOME = " $ADC_HOME
-sed 's|XX_JAVA_HOME_XX|'"${JAVA_HOME}"'|g' $ADC_HOME/conf/hadoop/hadoop-env.sh > $ADC_HADOOP/conf/hadoop_env.sh 
+echo "ADC_HADOOP = " $ADC_HADOOP
+sed 's|XX_JAVA_HOME_XX|'$JAVA_HOME'|g' $ADC_HOME/conf/hadoop/hadoop-env.sh > $ADC_HADOOP/conf/hadoop-env.sh 
 
 # hdfs-site.xml
 # XX_HADOOP_NAME_XX
